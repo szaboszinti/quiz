b5458e4f7df2b30a3b4a7f1095af85c1e76981e6
szaboszinti <szabo.szintia@diak.szbi-pg.hu>
2026-03-26 10:40:21 +0100

    télalap adatainak betöltése

---
+++ value.js
@@ -0,0 +1,7 @@
+function
+szamol(){
+    
+    let szelesseg = document.getElementById("szelesseg").value
+    let magassag = document.getElementById("magassag").value
+    console.log(szelesseg, magassag);
+}
